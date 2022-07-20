d8c2784d53138af9a46c71bbc2dcbe79628aafa8
beauverger <beauverger.rodolphe@orange.fr>
2022-07-20 11:36:11 +0200

    prestations - end

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,6 @@
 <div class="navbar bg-gold-texture">
   <div class="navbar-start">
-    <div class="dropdown">
+    <div class="dropdown text-grey-200">
       <label tabindex="0" class="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="black"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg>
       </label>

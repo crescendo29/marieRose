5c50f68fd7e6d5024f3e834b6bd4a84dfe8395bc
beauverger <beauverger.rodolphe@orange.fr>
2022-08-04 10:50:07 +0200

    modif cartes produits

--- src/routes/produits.svelte
+++ src/routes/produits.svelte
@@ -1,18 +1,19 @@
-<section class="bg-marieBlue h-screen pt-4 flex flex-col justify-center md:w-[1240px] mx-auto">
-  <div class="collapse mx-4 mt-4 rounded-xl">
+<!-- <section class="bg-marieBlue min-h-screen pt-4 flex flex-col md:flex-row justify-center md:w-[1240px] mx-auto">
+  <div class="collapse mx-4 mt-4 rounded-xl text-lg">
     <input type="checkbox" class="peer" /> 
-    <div class="collapse-title bg-gradient-to-r from-pinkepilo to-blueepilo text-white peer-checked:text-pink-100">
-      <h2>Les &Eacute;pilations EPILODERM®</h2>  
+    <div class="collapse-title bg-gradient-to-r from-pinkepilo to-black/50 text-white">
+      <img src="/images/epiloderm.webp" class="w-[250px]" alt="epiloderm">
+    
     </div>
-    <div class="collapse-content bg-gradient-to-r from-pinkepilo to-blueepilo text-white"> 
-      <div class="">
+    <div class="collapse-content bg-gradient-to-r from-pinkepilo to-black text-white"> 
+      <div class="my-4">
         <h3 class="mb-2">UN SOIN D'&Eacute;PILATION AU PLUS PROCHE DE LA NATURE</h3>
         <p class="mb-2">Les cires Epiloderm® sont fabriquées en France et élaborées à partir de résines de pin 100% d'origine végétale. L'ajout de certains ingrédients, nécessaires à sa souplesse et à son élasticité, représente un faible pourcentage.</p>
         <p class="mb-2">Les cires Epiloderm® sont et restent les cires à épiler les plus naturelles du marché Françaiset mondial (Hors cire bio, sucre...).</p>
         <p class="mb-2">Elles ne peuvent donc en aucun cas être comparées à une quelconque cire synthétique.</p>
         <p>Epiloderm® une repousse  plus lente, une peau plus douce, l'assurance d'un résultat d'exception !</p>
       </div>  
-      <div class="">
+      <div class="my-4">
         <h3 class="mb-2">COLOPHANE ?</h3>
         <p class="mb-2">La récolte de résine de pin végétale (le gemmage) permet d'extraire de la térébenthine et de la colophane. Ces deux ingrédients seront, après traitement, utilisés soit dans l'industrie, soit dans la fabrication de cosmétiques ou de cires à épiler.</p>
         <p>La colophane à l'état brut contenant certains allergènes, comme la plupart des ingrédients naturels, nécessite d'être estérifiée (Purifiée des éléments allergènes). Une fois estérifiée, elle devient un ester de colophane ou Glycéryl Rosinate (Appellation INCI).</p>
@@ -47,4 +48,62 @@
     </div>
   </div>
 </section>
+ -->
+<section class="bg-marieBlue min-h-screen pt-4 md:w-[1240px] mx-auto flex flex-col md:flex-row md:justify-around items-center justify-center">
+  <div class="">
+        <!-- The button to open modal epiloderm -->
+    <label for="my-modal-1" class="btn modal-button bg-transparent border-none hover:bg-transparent"><img src="/images/epiloderm.webp" class="w-[250px]" alt="epiloderm"></label>
 
+    <!-- Put this part before </body> tag -->
+    <input type="checkbox" id="my-modal-1" class="modal-toggle" />
+    <label for="my-modal-1" class="modal cursor-pointer">
+      <label class="modal-box relative bg-pinkepilo text-white" for="">
+        <div class="my-4">
+          <h3 class="mb-2">UN SOIN D'&Eacute;PILATION AU PLUS PROCHE DE LA NATURE</h3>
+          <p class="mb-2">Les cires Epiloderm® sont fabriquées en France et élaborées à partir de résines de pin 100% d'origine végétale. L'ajout de certains ingrédients, nécessaires à sa souplesse et à son élasticité, représente un faible pourcentage.</p>
+          <p class="mb-2">Les cires Epiloderm® sont et restent les cires à épiler les plus naturelles du marché Françaiset mondial (Hors cire bio, sucre...).</p>
+          <p class="mb-2">Elles ne peuvent donc en aucun cas être comparées à une quelconque cire synthétique.</p>
+          <p>Epiloderm® une repousse  plus lente, une peau plus douce, l'assurance d'un résultat d'exception !</p>
+        </div>  
+        <div class="my-4">
+          <h3 class="mb-2">COLOPHANE ?</h3>
+          <p class="mb-2">La récolte de résine de pin végétale (le gemmage) permet d'extraire de la térébenthine et de la colophane. Ces deux ingrédients seront, après traitement, utilisés soit dans l'industrie, soit dans la fabrication de cosmétiques ou de cires à épiler.</p>
+          <p>La colophane à l'état brut contenant certains allergènes, comme la plupart des ingrédients naturels, nécessite d'être estérifiée (Purifiée des éléments allergènes). Une fois estérifiée, elle devient un ester de colophane ou Glycéryl Rosinate (Appellation INCI).</p>
+        </div>  
+        <div class="my-4">
+          <h3 class="mb-2">SANS COLOPHANE</h3>
+          <p class="underline mb-2">La Cire Plastique c'est pas Fantastique</p>
+          <p class="mb-2">Une Cire sans colophane est une cire de synthèse entièrement synthétique et élaborée à partir de produits pétrochimiques donc de plastique.</p>
+          <p>Une cire sans colophane est toujours parfumée avec des senteurs puissantes et entêtantes afin de masquer l'odeur du plastique brûlé au moment de la fusion qui, dans le cas contraire, serait tout à fait insupportable.</p>
+        </div>  
+      </label>
+    </label>
+  </div>
+  
+
+<div class="bg-gradient-to-r from-phytomer to-phytomerClear flex items-center justify-center">
+  <!-- The button to open modal Phytomer -->
+  <label for="my-modal-2" class="btn modal-button uppercase bg-gradient-to-r from-phytomer to-phytomerClear text-marieBlue h-[350px] w-[250px] border-none">Phytomer</label>
+
+  <!-- Put this part before </body> tag -->
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+  <label for="my-modal-2" class="modal cursor-pointer">
+    <label class="modal-box relative bg-gradient-to-r from-phytomer to-phytomerClear text-marieBlue" for="">
+      <div class="">
+        <h3 class="mb-2">L’AMOUR DE LA BRETAGNE</h3>
+        <p class="mb-2">Notre Laboratoire est installé à quelques kilomètres de Saint-Malo, dans la magnifique baie du Mont Saint-Michel. C’est un centre de recherche à la pointe de la cosmétique marine et le lieu où nous fabriquons tous nos produits à destination du monde entier.</p>
+      </div>  
+      <div class="my-4">
+        <h3 class="mb-2">LE BIEN-ÊTRE DANS LA PEAU</h3>
+        <p class="mb-2">Nos massages et nos soins sont pratiqués dans les spas du monde entier. Ils redonnent à votre peau la fraîcheur et l’éclat qu’elle mérite tout en vous offrant une parenthèse profondément ressourçante.</p>
+      </div>  
+      <div class="">
+        <h3 class="mb-2">LE RESPECT DE LA NATURE</h3>
+        <p class="mb-2">Nous pratiquons une cosmétique raisonnée en privilégiant à chaque étape de production, des méthodes dont l'impact environnemental est limité. Nous cultivons nos algues et nos plantes bio et inventons des solutions responsables pour une beauté respectueuse de la peau et de la nature.</p>
+      </div>  
+    </label>
+  </label>
+
+</div>
+
+</section>
